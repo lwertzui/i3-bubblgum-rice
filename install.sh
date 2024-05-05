27433079458fcd92f771da96f0903a7e26a50d0c
wertlzui <122686226+lwertzui@users.noreply.github.com>
2024-05-05 12:54:17 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#cp ../configs/i3/config ~/.config/i3/config
+echo "copying i3 config"
+
+#cp ../configs/terminator/config ~/.config/terminator/config
+echo "copying terminator config"
+
+#cp ../configs/polybar/config.ini ~/.config/polybar/config.ini
+echo "copying polybar config"
+
+#cp ../configs/picom/config.ini ~/.config/picom/config.ini
+echo "copying picom config"
+
+#cp ../configs/rofi/config.rasi ~/.config/rofi.rasi
+echo "copying rofi config"
+
+#cp ../configs/xborder/config.json ~/.config/xborder/config.json
+echo "copying xborder config"
